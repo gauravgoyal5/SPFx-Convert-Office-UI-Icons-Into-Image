67efbc247a18288cd259c692af54810e9d505745
gauravgoyal_5@yahoo.com <gauravgoyal_5@yahoo.com>
2019-06-14 23:35:27 +0530

    first commit

---
+++ src/webparts/convertOfficeUiIconsIntoImage/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IConvertOfficeUiIconsIntoImageWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ConvertOfficeUiIconsIntoImageWebPartStrings' {
+  const strings: IConvertOfficeUiIconsIntoImageWebPartStrings;
+  export = strings;
+}
